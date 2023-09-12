d2427846b26714fd48d08fdbe86818792aa754c1
eli.huang <eli.huang@frog-jump.com>
2023-09-12 21:36:54 +0800

    Initialize Spring Boot Project

---
+++ src/test/java/com/owl/aipartner/AipartnerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.owl.aipartner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AipartnerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
