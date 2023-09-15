b90758d41b86f7e3ac44e47d9117e9408a7195b3
eli.huang <eli.huang@frog-jump.com>
2023-09-15 17:08:35 +0800

    Integration Test MvcResult

--- src/test/java/com/owl/aipartner/UserTest.java
+++ src/test/java/com/owl/aipartner/UserTest.java
@@ -1,44 +1,72 @@
 package com.owl.aipartner;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.json.JSONArray;
 import org.json.JSONObject;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import com.owl.aipartner.model.po.UserPO;
+import com.owl.aipartner.repository.mongo.UserRepository;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @AutoConfigureMockMvc
 public class UserTest {
 
+        private HttpHeaders httpHeaders;
+
         @Autowired
         private MockMvc mockMvc;
 
+        @Autowired
+        private UserRepository userRepository;
+
+        @Before
+        public void init() {
+                httpHeaders = new HttpHeaders();
+                httpHeaders.add(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE);
+        }
+
+        @After
+        public void clear() {
+                userRepository.deleteAll();
+        }
+
         @Test
         public void testCreateUser() throws Exception {
-                HttpHeaders httpHeaders = new HttpHeaders();
-                httpHeaders.add(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE);
-
                 JSONObject request = new JSONObject()
+                                .put("id", "1")
                                 .put("name", "Mike")
                                 .put("age", 18);
 
                 RequestBuilder requestBuilder = MockMvcRequestBuilders.post("/users")
                                 .headers(httpHeaders)
                                 .content(request.toString());
-
+                // 初次新增
                 mockMvc.perform(requestBuilder)
                                 .andDo(print())
                                 .andExpect(status().isCreated())
@@ -47,6 +75,82 @@ public class UserTest {
                                 .andExpect(jsonPath("$.age").value(request.get("age")))
                                 .andExpect(header().exists(HttpHeaders.LOCATION))
                                 .andExpect(header().string(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE));
+                // 若 ID 重複
+                mockMvc.perform(requestBuilder)
+                                .andExpect(status().isUnprocessableEntity());
+        }
+
+        @Test
+        public void testGetUser() throws Exception {
+                UserPO user = new UserPO(null, "Mike", 22);
+                userRepository.insert(user);
+
+                mockMvc.perform(get("/users/" + user.getId())
+                                .headers(httpHeaders))
+                                .andExpect(status().isOk())
+                                .andExpect(jsonPath("$.id").value(user.getId()))
+                                .andExpect(jsonPath("$.name").value(user.getName()))
+                                .andExpect(jsonPath("$.age").value(user.getAge()));
+        }
+
+        @Test
+        public void testReplaceUser() throws Exception {
+                UserPO user = new UserPO(null, "Mike", 22);
+                userRepository.insert(user);
+
+                JSONObject request = new JSONObject()
+                                .put("name", "Ted")
+                                .put("age", 14);
+
+                mockMvc.perform(put("/users/" + user.getId())
+                                .headers(httpHeaders)
+                                .content(request.toString()))
+                                .andExpect(status().isOk())
+                                .andExpect(jsonPath("$.id").value(user.getId()))
+                                .andExpect(jsonPath("$.name").value(request.getString("name")))
+                                .andExpect(jsonPath("$.age").value(request.getInt("age")));
+        }
+
+        @Test(expected = RuntimeException.class)
+        public void testDeleteUser() throws Exception {
+                UserPO user = new UserPO(null, "Mike", 22);
+                userRepository.insert(user);
+
+                mockMvc.perform(delete("/users/" + user.getId())
+                                .headers(httpHeaders))
+                                .andExpect(status().isNoContent());
+
+                userRepository.findById(user.getId())
+                                .orElseThrow(RuntimeException::new);
+        }
+
+        @Test
+        public void testSearchUsersSortByNameAsc() throws Exception {
+                UserPO u1 = new UserPO("1", "Apple", 26);
+                UserPO u2 = new UserPO("2", "Banana", 100);
+                UserPO u3 = new UserPO("3", "Cat", 91);
+                UserPO u4 = new UserPO("4", "Dog", 72);
+                UserPO u5 = new UserPO("5", "Eagle", 51);
+                userRepository.insert(List.of(u1, u2, u3, u4, u5));
+
+                MvcResult result = mockMvc.perform(get("/users")
+                                .headers(httpHeaders)
+                                .param("name", "l")
+                                .param("orderBy", "name")
+                                .param("sortRule", "asc"))
+                                .andReturn();
+                MockHttpServletResponse mockHttpResp = result.getResponse();
+                String respJson = mockHttpResp.getContentAsString();
+                JSONArray userJsonArray = new JSONArray(respJson);
+
+                Assert.assertEquals(2, userJsonArray.length());
+                Assert.assertEquals(u1.getId(), userJsonArray.getJSONObject(0).getString("id"));
+                Assert.assertEquals(u5.getId(), userJsonArray.getJSONObject(1).getString("id"));
+
+                Assert.assertEquals(HttpStatus.OK.value(), mockHttpResp.getStatus());
+                Assert.assertEquals(MediaType.APPLICATION_JSON_VALUE,
+                                mockHttpResp.getHeader(HttpHeaders.CONTENT_TYPE));
 
         }
+
 }
